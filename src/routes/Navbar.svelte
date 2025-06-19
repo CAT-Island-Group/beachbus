9f82179acfb1b61b3b50e9005c05cc7e19f4a340
Jared Hechanova <jlhechanova@gmail.com>
2025-06-20 00:21:25 +0800

    feat: added verification

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from "$app/state";
+</script>
+
+<div class="flex gap-8">
+    <a href="/registration" class={`text-lg ${page.url.pathname === '/registration' ? "active" : ""}`}>Registration</a>
+    <a href="/verification" class={`text-lg ${page.url.pathname === '/verification' ? "active" : ""}`}>Verification</a>
+</div>
+
+<style>
+    .active {
+        font-weight: bold;
+        color: dodgerblue;
+        border-bottom: 2px solid dodgerblue;
+    }
+</style>
