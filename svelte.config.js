3f9b71b274b169536ef5f8e6387cb6e0ee6ce5ce
Jared Hechanova <jlhechanova@gmail.com>
2025-06-22 13:10:09 +0800

    refactor: change to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
 	}
 };
 
