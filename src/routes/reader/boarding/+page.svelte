c069d781ca75bc03803a766ef4224781804b708e
Jared Hechanova <jlhechanova@gmail.com>
2025-07-03 18:09:33 +0800

    style: change boarding and activation module headings

--- src/routes/reader/boarding/+page.svelte
+++ src/routes/reader/boarding/+page.svelte
@@ -50,7 +50,7 @@
     <div class="w-90 mx-auto py-12 flex flex-col justify-center items-center gap-4 rounded-lg shadow-lg">
         <div class="w-full p-8 flex flex-col items-center">
             <h1 class="text-xl font-bold">BeachBus Reader</h1>
-            <h2 class="border-b border-sky-600 text-sky-600 text-lg font-bold">Checker</h2>
+            <h2 class="border-b border-sky-600 text-sky-600 text-lg font-bold">Boarding</h2>
             <div class="relative my-16 flex justify-center items-center size-40">
                 {#if nfc?.isPending}
                     <svg class="absolute size-16 animate-spin text-sky-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
